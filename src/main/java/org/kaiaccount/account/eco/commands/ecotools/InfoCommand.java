eafaaf4c18c13be17dd5f241aa9052d50aeae94c
Mose <nekronmoseley@gmail.com>
2024-01-07 10:46:00 +0000

    Adds Gradle (#1)
    * Gradle

--- src/main/java/org/kaiaccount/account/eco/commands/ecotools/InfoCommand.java
+++ src/main/java/org/kaiaccount/account/eco/commands/ecotools/InfoCommand.java
@@ -4,7 +4,6 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
 import org.jetbrains.annotations.NotNull;
 import org.kaiaccount.AccountInterface;
@@ -12,7 +11,6 @@ import org.kaiaccount.account.eco.EcoToolPlugin;
 import org.kaiaccount.account.inter.currency.Currency;
 import org.kaiaccount.account.inter.event.TransactionCompletedEvent;
 import org.kaiaccount.account.inter.event.TransactionEvent;
-import org.kaiaccount.account.inter.vault.VaultPluginWrapper;
 import org.mose.command.ArgumentCommand;
 import org.mose.command.CommandArgument;
 import org.mose.command.arguments.operation.ExactArgument;
@@ -24,72 +22,65 @@ import java.util.Optional;
 
 public class InfoCommand implements ArgumentCommand {
 
-	public static final CommandArgument<String> INFO = new ExactArgument("info");
+    public static final CommandArgument<String> INFO = new ExactArgument("info");
 
-	@Override
-	public @NotNull List<CommandArgument<?>> getArguments() {
-		return List.of(INFO);
-	}
+    @Override
+    public @NotNull List<CommandArgument<?>> getArguments() {
+        return List.of(INFO);
+    }
 
-	@Override
-	public @NotNull String getDescription() {
-		return "Info about EcoTool";
-	}
+    @Override
+    public @NotNull String getDescription() {
+        return "Info about EcoTool";
+    }
 
-	@Override
-	public @NotNull Optional<String> getPermissionNode() {
-		return Optional.empty();
-	}
+    @Override
+    public @NotNull Optional<String> getPermissionNode() {
+        return Optional.empty();
+    }
 
-	@Override
-	public boolean run(CommandContext commandContext, String... args) {
-		CommandSender sender = commandContext.getSource();
-		Collection<Currency<?>> currencies = AccountInterface.getManager().getCurrencies();
-		RegisteredListener[] transactionEventHooks = TransactionEvent.getHandlerList().getRegisteredListeners();
-		RegisteredListener[] transactionCompletedHooks =
-				TransactionCompletedEvent.getHandlerList().getRegisteredListeners();
-		Plugin plugin = Bukkit.getPluginManager().getPlugin("Vault");
-		boolean isVaultInstalled = false;
-		if (plugin != null) {
-			isVaultInstalled = plugin instanceof VaultPluginWrapper;
-		}
+    @Override
+    public boolean run(CommandContext commandContext, String... args) {
+        CommandSender sender = commandContext.getSource();
+        Collection<Currency<?>> currencies = AccountInterface.getManager().getCurrencies();
+        RegisteredListener[] transactionEventHooks = TransactionEvent.getHandlerList().getRegisteredListeners();
+        RegisteredListener[] transactionCompletedHooks =
+                TransactionCompletedEvent.getHandlerList().getRegisteredListeners();
+        sender.sendMessage("|===|Info|===|");
+        sender.sendMessage("Version: " + EcoToolPlugin.getPlugin().getDescription().getVersion());
+        sender.sendMessage("Vault service enabled: " + Bukkit.getServicesManager().isProvidedFor(Economy.class));
+        sender.sendMessage("Currencies: " + currencies.size());
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("| Plugin | Key name | Symbol |");
+            for (Currency<?> currency : currencies) {
+                sender.sendMessage("| "
+                        + currency.getPlugin().getName()
+                        + " | "
+                        + currency.getKeyName()
+                        + " | "
+                        + currency.getSymbol()
+                        + " |");
+            }
+        }
+        sender.sendMessage("TransactionCompleted Hooks: " + transactionCompletedHooks.length);
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("| Plugin | Listener class |");
+            for (RegisteredListener listener : transactionCompletedHooks) {
+                sender.sendMessage("| " + listener.getPlugin().getName() + " | " + listener.getListener()
+                        .getClass()
+                        .getSimpleName() + " |");
+            }
+        }
 
-		sender.sendMessage("|===|Info|===|");
-		sender.sendMessage("Version: " + EcoToolPlugin.getPlugin().getDescription().getVersion());
-		sender.sendMessage("Vault plugin enabled: " + isVaultInstalled);
-		sender.sendMessage("Vault service enabled: " + Bukkit.getServicesManager().isProvidedFor(Economy.class));
-		sender.sendMessage("Currencies: " + currencies.size());
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("| Plugin | Key name | Symbol |");
-			for (Currency<?> currency : currencies) {
-				sender.sendMessage("| "
-						+ currency.getPlugin().getName()
-						+ " | "
-						+ currency.getKeyName()
-						+ " | "
-						+ currency.getSymbol()
-						+ " |");
-			}
-		}
-		sender.sendMessage("TransactionCompleted Hooks: " + transactionCompletedHooks.length);
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("| Plugin | Listener class |");
-			for (RegisteredListener listener : transactionCompletedHooks) {
-				sender.sendMessage("| " + listener.getPlugin().getName() + " | " + listener.getListener()
-						.getClass()
-						.getSimpleName() + " |");
-			}
-		}
-
-		sender.sendMessage("Transaction Hooks: " + transactionEventHooks.length);
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("| Plugin | Listener class |");
-			for (RegisteredListener listener : transactionEventHooks) {
-				sender.sendMessage("| " + listener.getPlugin().getName() + " | " + listener.getListener()
-						.getClass()
-						.getSimpleName() + " |");
-			}
-		}
-		return true;
-	}
+        sender.sendMessage("Transaction Hooks: " + transactionEventHooks.length);
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("| Plugin | Listener class |");
+            for (RegisteredListener listener : transactionEventHooks) {
+                sender.sendMessage("| " + listener.getPlugin().getName() + " | " + listener.getListener()
+                        .getClass()
+                        .getSimpleName() + " |");
+            }
+        }
+        return true;
+    }
 }
