958085a19ccf951a84d31a67959b659ef17f702a
loudnessofthelambs <hirad.dehghani@gmail.com>
2024-01-22 11:10:31 -0500

    seriously fixed the last commit, for the last time now, im serious

--- src/main/java/org/kaiaccount/account/eco/commands/bank/DeletePlayerBankAccountCommand.java
+++ src/main/java/org/kaiaccount/account/eco/commands/bank/DeletePlayerBankAccountCommand.java
@@ -3,8 +3,10 @@ package org.kaiaccount.account.eco.commands.bank;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 import org.kaiaccount.AccountInterface;
+import org.kaiaccount.account.eco.commands.argument.account.PlayerBankArgument;
 import org.kaiaccount.account.eco.message.Messages;
 import org.kaiaccount.account.eco.message.messages.error.SourceOnlyCommandMessage;
+import org.kaiaccount.account.eco.permission.Permissions;
 import org.kaiaccount.account.inter.type.named.bank.player.PlayerBankAccount;
 import org.kaiaccount.account.inter.type.player.PlayerAccount;
 import org.mose.command.ArgumentCommand;
@@ -12,18 +14,21 @@ import org.mose.command.CommandArgument;
 import org.mose.command.arguments.operation.ExactArgument;
 import org.mose.command.arguments.simple.text.StringArgument;
 import org.mose.command.context.CommandContext;
-
+import org.mose.command.arguments.operation.permission.PermissionOrArgument;
 import java.util.List;
 import java.util.Optional;
 
 public class DeletePlayerBankAccountCommand implements ArgumentCommand {
 
-    public static final ExactArgument CREATE = new ExactArgument("close");
-    public static final StringArgument NAME = new StringArgument("name");
+    public static final ExactArgument CLOSE = new ExactArgument("close");
+    public static final PlayerBankArgument BANK = new PermissionOrArgument<>("bank",
+            sender -> (sender.hasPermission(
+                    Permissions.BALANCE_OTHER.getPermissionNode())),
+            PlayerBankArgument.allPlayerBanks("bank"), PlayerBankArgument.senderBanks("bank"));
 
     @Override
     public @NotNull List<CommandArgument<?>> getArguments() {
-        return List.of(CREATE, NAME);
+        return List.of(CLOSE, BANK);
     }
 
     @Override
@@ -33,24 +38,28 @@ public class DeletePlayerBankAccountCommand implements ArgumentCommand {
 
     @Override
     public @NotNull Optional<String> getPermissionNode() {
-        return Optional.empty();
+        return Optional.of(Permissions.CREATE_BANK_ACCOUNT.getPermissionNode());
     }
 
     @Override
     public boolean run(CommandContext commandContext, String... strings) {
-        String bankName = commandContext.getArgument(this, NAME);
+        PlayerBankAccount bank = commandContext.getArgument(this, BANK);
         if (!(commandContext.getSource() instanceof OfflinePlayer player)) {
             commandContext.getSource()
                     .sendMessage(Messages.SOURCE_ONLY.getProcessedMessage(SourceOnlyCommandMessage.PLAYER_SOURCE));
             return true;
         }
-        PlayerAccount<?> account = AccountInterface.getManager().getPlayerAccount(player);
-        if (!account.getBank(bankName).isPresent()) {
+        if (!bank.isPresent()) {
             commandContext.getSource().sendMessage("Bank does not exist");
             return true;
         }
-        PlayerBankAccount bankAccount = account.deleteBankAccount(bankName);
-        commandContext.getSource().sendMessage("Created " + bankAccount.getAccountName());
+        PlayerAccount<?> account = bank.getAccountHolder();
+		account.deposit(bank.getBalance());
+        for (UUID acc : bank.getAccounts().keySet()) {
+            bank.removeAccount(acc);
+        }
+
+        commandContext.getSource().sendMessage("deleted " + bank.getAccountName());
         return true;
     }
 }
